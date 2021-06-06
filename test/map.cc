e5b17b567d4281629216f0c3fb122cf8303674e1
Arindam Das <dasarindam.mails@gmail.com>
2021-06-06 13:24:19 +0530

    Refactored tests.

--- test/map.cc
+++ test/map.cc
@@ -4,15 +4,10 @@
 #include <thread>
 #include <chrono>
 
-TEST(MapTest, Constructs)
-{
-    silicondb::map<int, int> map;
-}
-
-TEST(MapTest, Consistent)
+template <typename integer_map>
+void test_map_consistency(integer_map &map)
 {
     using namespace std::chrono_literals;
-    silicondb::map<int, int> map;
 
     // random sleep durations
     auto d1 = 276ms, d2 = 532ms;
@@ -38,47 +33,41 @@ TEST(MapTest, Consistent)
     put_thread_1.join();
     put_thread_2.join();
 
-    std::thread get_thread_1([&]()
-                             {
-                                 EXPECT_EQ(map.get(1), 2);
-                                 std::this_thread::sleep_for(d1);
+    auto get_thread_task = [&]()
+    {
+        EXPECT_EQ(map.get(1), 2);
+        std::this_thread::sleep_for(d1);
 
-                                 EXPECT_EQ(map.get(2), 3);
-                                 std::this_thread::sleep_for(d2);
+        EXPECT_EQ(map.get(2), 3);
+        std::this_thread::sleep_for(d2);
 
-                                 EXPECT_EQ(map.get(3), 4);
-                                 std::this_thread::sleep_for(d1);
+        EXPECT_EQ(map.get(3), 4);
+        std::this_thread::sleep_for(d1);
 
-                                 EXPECT_EQ(map.get(5), 6);
-                                 std::this_thread::sleep_for(d2);
+        EXPECT_EQ(map.get(5), 6);
+        std::this_thread::sleep_for(d2);
 
-                                 EXPECT_EQ(map.get(7), 8);
+        EXPECT_EQ(map.get(7), 8);
 
-                                 std::this_thread::sleep_for(d1);
+        std::this_thread::sleep_for(d1);
 
-                                 EXPECT_EQ(map.get(9), 10);
-                             });
+        EXPECT_EQ(map.get(9), 10);
+    };
 
-    std::thread get_thread_2([&]()
-                             {
-                                 EXPECT_EQ(map.get(1), 2);
-                                 std::this_thread::sleep_for(d1);
-
-                                 EXPECT_EQ(map.get(2), 3);
-                                 std::this_thread::sleep_for(d2);
-
-                                 EXPECT_EQ(map.get(3), 4);
-                                 std::this_thread::sleep_for(d1);
-
-                                 EXPECT_EQ(map.get(5), 6);
-                                 std::this_thread::sleep_for(d2);
-
-                                 EXPECT_EQ(map.get(7), 8);
-                                 std::this_thread::sleep_for(d1);
-
-                                 EXPECT_EQ(map.get(9), 10);
-                             });
+    std::thread get_thread_1(get_thread_task);
+    std::thread get_thread_2(get_thread_task);
 
     get_thread_1.join();
     get_thread_2.join();
+}
+
+TEST(MapTest, Constructs)
+{
+    silicondb::map<int, int> map;
+}
+
+TEST(MapTest, Consistent)
+{
+    silicondb::map<int, int> map;
+    test_map_consistency(map);
 }
